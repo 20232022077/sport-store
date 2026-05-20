e53810359fe9ad63644bd320214b8d878a70ced6
OSAMA <batitcr7@gmail.com>
2026-05-20 22:31:57 +0300

    Add Jetstream authentication and database setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,18 @@
 <h1>🏀 Welcome to Sport Store</h1>
 <p>First page is working successfully 🎉</p>
 
+@if (Route::has('login'))
+    <div>
+        @auth
+            <a href="{{ url('/dashboard') }}">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+
 </body>
 </html>
