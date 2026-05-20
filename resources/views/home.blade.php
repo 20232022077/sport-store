cf7eb196cf33f477bb1852bd79672114c98902a0
OSAMA <batitcr7@gmail.com>
2026-05-20 16:22:55 +0300

    Initial Laravel project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sport Store</title>
+</head>
+<body>
+
+<h1>🏀 Welcome to Sport Store</h1>
+<p>First page is working successfully 🎉</p>
+
+</body>
+</html>
